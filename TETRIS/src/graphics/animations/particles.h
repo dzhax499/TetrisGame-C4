df69d418dc6520ff1b28157e8028afb2dffec29f
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-03-03 08:17:47 +0700

    Update particles.h

--- TETRIS/src/graphics/animations/particles.h
+++ TETRIS/src/graphics/animations/particles.h
@@ -1,8 +1,25 @@
 #ifndef PARTICLES_H
 #define PARTICLES_H
 
-void showScorePopupAnimation(int score);
-void showComboEffect(int combo);
-void showLevelUpEffect();
+#include <stdbool.h>
+
+// Struktur data untuk efek visual
+typedef struct {
+    int x, y;
+    int lifetime;
+    bool active;
+} ParticleEffect;
+
+// Inisialisasi efek
+void initParticles(ParticleEffect particles[], int maxParticles);
+
+// Menampilkan animasi skor popup
+void spawnScorePopup(ParticleEffect particles[], int x, int y, int score);
+
+// Menampilkan animasi level up
+void spawnLevelUpEffect(ParticleEffect particles[], int x, int y);
+
+// Update efek animasi
+void updateParticles(ParticleEffect particles[], int maxParticles);
 
 #endif
