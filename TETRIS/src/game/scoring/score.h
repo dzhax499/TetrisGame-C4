8bf0cfdae6e33e87a2272bdb4988de8b3dd8fde5
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-03-03 08:15:26 +0700

    Update score.h

--- TETRIS/src/game/scoring/score.h
+++ TETRIS/src/game/scoring/score.h
@@ -1,10 +1,23 @@
 #ifndef SCORE_H
 #define SCORE_H
 
-#include "game_state.h"
+#include <stdbool.h>
 
-void initScore(GameState *state);
-void updateScore(GameState *state, int linesCleared);
-void applyComboBonus(GameState *state);
+// Struktur data untuk skor
+typedef struct {
+    int score;
+    int combo;
+    int level;
+    int linesCleared;
+} ScoreSystem;
 
-#endif 
+// Inisialisasi sistem skor
+void initScoreSystem(ScoreSystem *scoreSys);
+
+// Update skor berdasarkan jumlah garis yang dihapus
+void updateScore(ScoreSystem *scoreSys, int linesCleared, bool tetrisBonus, bool perfectClear);
+
+// Reset sistem skor
+void resetScore(ScoreSystem *scoreSys);
+
+#endif
