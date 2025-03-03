8e8973b20d20f51157ef24482bb55e64725e435a
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-03-03 08:16:45 +0700

    Update timer.h

--- TETRIS/src/utils/timer.h
+++ TETRIS/src/utils/timer.h
@@ -1,10 +1,26 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include "game_state.h"
+#include <stdint.h>
 
-void initTimer(GameState *state);
-void updateTimer(GameState *state, float deltaTime);
-void displayFormattedTime(float time);
+// Struktur data timer
+typedef struct {
+    uint32_t startTime;
+    uint32_t elapsedTime;
+    bool isPaused;
+} GameTimer;
+
+// Inisialisasi timer
+void initTimer(GameTimer *timer);
+
+// Update waktu saat game berjalan
+void updateTimer(GameTimer *timer, uint32_t currentTime);
+
+// Pause dan resume timer
+void pauseTimer(GameTimer *timer);
+void resumeTimer(GameTimer *timer);
+
+// Mendapatkan waktu yang sudah berjalan
+uint32_t getElapsedTime(GameTimer *timer);
 
 #endif
