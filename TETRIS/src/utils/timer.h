2844b396d41f8ee729e57c8da183fe683272b375
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-03-03 08:08:06 +0700

    Update timer.h

--- TETRIS/src/utils/timer.h
+++ TETRIS/src/utils/timer.h
@@ -1,1 +1,10 @@
+#ifndef TIMER_H
+#define TIMER_H
 
+#include "game_state.h"
+
+void initTimer(GameState *state);
+void updateTimer(GameState *state, float deltaTime);
+void displayFormattedTime(float time);
+
+#endif
