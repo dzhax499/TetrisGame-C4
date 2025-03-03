66f2828220e05f04cb2de7f2e13a8e08bdf0184c
RizkySatria123 <rizky12satria@gmail.com>
2025-03-03 09:02:00 +0700

    Update main.c

--- TETRIS/src/main.c
+++ TETRIS/src/main.c
@@ -1,1 +1,12 @@
+// Nama Program : main.c
+// Deskripsi    : 
+// Pembuat      : [Rizky Satria Gunawan, 241511089, 3 Maret 2025, 8:52]
 
+#include "game/game.h"
+
+int main() {
+    GameState game;
+    game_init(&game); // Inisialisasi game
+    game_run(&game);  // Jalankan game loop
+    return 0;
+}
