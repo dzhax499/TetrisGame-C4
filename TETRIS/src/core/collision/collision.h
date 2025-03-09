6003fd8bd35eaad42e0e8d486fbf2d2a3f5bc31a
dzhax499 <dzdzakir@gmail.com>
2025-03-09 19:12:16 +0700

    definisi awal collision.h

---
+++ TETRIS/src/core/collision/collision.h
@@ -0,0 +1,16 @@
+/**
+ * @file collision.h
+ * @brief Header file untuk deteksi collision tetromino
+ * @author Dzakit Tsabit Assyafiq
+ * 
+ * File ini berisi fungsi-fungsi untuk mendeteksi collision antara
+ * tetromino dengan grid, wall, dan floor.
+ */
+
+ #ifndef COLLISION_H
+ #define COLLISION_H
+ 
+ #include "../tetromino/blocks.h"
+ #include <stdbool.h>
+ 
+ #endif
