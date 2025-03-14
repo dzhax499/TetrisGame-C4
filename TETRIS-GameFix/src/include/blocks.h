e980977717d154fec46ce10c4f96a875c8450e2b
dzhax499 <dzdzakir@gmail.com>
2025-03-14 07:29:47 +0700

    perbaikan blocks.c/h dan tetris.h ifdef menjadi ifndef, dan penereapan struktur

--- TETRIS-GameFix/src/include/blocks.h
+++ TETRIS-GameFix/src/include/blocks.h
@@ -2,7 +2,7 @@
 // Deskripsi : Header untuk blok-blok game Tetris  
 // Oleh      : Dzakit Tsabit 241511071
 
-#ifdef BLOCKS_H
+#ifndef BLOCKS_H
 #define BLOCKS_H
 
 #include "tetris.h"
@@ -17,8 +17,8 @@ void RotateBlock(TetrisBlock* block);
 void MoveBlock(TetrisBlock* block, int x, int y);
 bool CheckCollision(TetrisBlock* block, int x, int y);
 void LockBlock(TetrisBlock* block);
-
-
+void ClearLines(void);
+void DrawBlock(TetrisBlock* block);
 
 
 #endif
