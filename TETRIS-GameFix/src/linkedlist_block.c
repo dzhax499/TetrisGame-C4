c852abdc7b95291ce06038ea75ecc21eab2f4d98
dzhax499 <dzdzakir@gmail.com>
2025-05-12 21:09:18 +0700

    Update linkedlist_block.c

--- TETRIS-GameFix/src/linkedlist_block.c
+++ TETRIS-GameFix/src/linkedlist_block.c
@@ -3,26 +3,30 @@
 
 void insert_AktifBlok(AktifBlok* list, TetrisBlock newBlock)
 {
-    BlockNode* newNode = (BlockNode*)malloc(sizeof(BlockNode));
-    newNode->block = newBlock;
-    newNode->next = NULL;
-    newNode->prev = NULL;
+    BlockNode* NodeBaru;
+    NodeBaru = (BlockNode*)malloc(sizeof(BlockNode));
+    NodeBaru->block = newBlock;
+    NodeBaru->next = NULL;
+    NodeBaru->prev = NULL;
 
     if (list->head == NULL) {
-        list->head = newNode;
-        list->tail = newNode;
+        list->head = NodeBaru;
+        list->tail = NodeBaru;
     } else {
-        list->tail->next = newNode;
-        newNode->prev = list->tail;
-        list->tail = newNode;
+        list->tail->next = NodeBaru;
+        NodeBaru->prev = list->tail;
+        list->tail = NodeBaru;
     }
 }
 
 void update_semuablok(AktifBlok* list, TetrisBoard* board)
 {
-    BlockNode* Pcur=list->head;
+    BlockNode* Pcur;
+    Pcur=list->head;
+
     while (Pcur != NULL) {
-        TetrisBlock* blk = &Pcur->block;
+        TetrisBlock* blk;
+        blk = &Pcur->block;
         
         for (int i = 0; i < 4 ; i++) {
             for (int j = 0; j < blk->ukuranblok; j++) {
@@ -31,7 +35,7 @@ void update_semuablok(AktifBlok* list, TetrisBoard* board)
                     int y = blk->y + i;
 
                     if (y >= 0 && y < BOARD_HEIGHT && x >= 0 && x < BOARD_WIDTH) {
-                        board->grid[y][x] = blk->color;  // atau kode warna/textur
+                        board->grid[y][x] = blk->type;
                     }
                 }
             }
